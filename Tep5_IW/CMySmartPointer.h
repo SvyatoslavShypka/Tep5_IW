505e93ad87b561ecd2590ebe87f2b839b52ce6d7
Svyatoslav Shypka <svyat200909@gmail.com>
2023-12-17 10:01:54 +0100

    006

--- Tep5_IW/CMySmartPointer.h
+++ Tep5_IW/CMySmartPointer.h
@@ -1,14 +1,64 @@
-#pragma once
-#include <CSellData.h>
+using namespace std;
 
-class CMySmartPointer
+class CRefCounter
 {
 public:
-	string x;
-	CMySmartPointer(CSellData* pcPointer) { pc_pointer = pcPointer; }
-	~CMySmartPointer() { delete pc_pointer; }
-	CSellData& operator*() { return(*pc_pointer); }
-	CSellData* operator->() { return(pc_pointer); }
+	CRefCounter() {
+		iAdd();
+	}
+	int iAdd() { return(++i_count); }
+	int iDec() { return(--i_count); };
+	int iGet() { return(i_count); }
 private:
-	CSellData* pc_pointer;
+	int i_count = 0;
+};//class CRefCounter
+
+template <typename T> class CMySmartPointer
+{
+public:
+	CMySmartPointer(T* pcPointer);
+	CMySmartPointer(const CMySmartPointer& pcOther);
+	~CMySmartPointer();
+	T& operator*() { return(*pc_pointer) };
+	T* operator->() { return(pc_pointer) };
+	CMySmartPointer& operator=(const CMySmartPointer& pcOther);
+private:
+	CRefCounter* pc_counter;
+	T* pc_pointer;
 };//class CMySmartPointer
+
+template <typename T> CMySmartPointer<T>::CMySmartPointer(T* pcPointer)
+{
+	pc_pointer = pcPointer;
+	pc_counter = new CRefCounter();
+	//pc_counter->iAdd();
+}//CMySmartPointer(T* pcPointer)
+
+template <typename T> CMySmartPointer<T>::CMySmartPointer(const CMySmartPointer& pcOther)
+{
+	pc_pointer = pcOther.pc_pointer;
+	pc_counter = pcOther.pc_counter;
+	pc_counter->iAdd();
+}//CMySmartPointer(const CMySmartPointer& pcOther)
+
+template <typename T> CMySmartPointer<T>::~CMySmartPointer()
+{
+	if (pc_counter->iDec() == 0)
+	{
+		delete pc_pointer;
+		delete pc_counter;
+	}//if (pc_counter->iDec())
+}//~CMySmartPointer()
+
+template <typename T> CMySmartPointer<T>& CMySmartPointer<T>::operator=(const CMySmartPointer& pcOther)
+{
+	if (pc_counter->iDec() == 0)
+	{
+		delete pc_pointer;
+		delete pc_counter;
+	}
+	pc_pointer = pcOther.pc_pointer;
+	pc_counter = pcOther.pc_counter;
+	pc_counter->iAdd();
+	return(*this);
+}
